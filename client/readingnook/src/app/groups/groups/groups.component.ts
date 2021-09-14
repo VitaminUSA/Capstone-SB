7c1e8e96597593984196d658d57dcd4d2dd14c93
Stephen Bowen <drew.bowen@jbhunt.com>
2021-09-14 16:43:40 -0500

    Continue Fleshing out group-tiles

--- client/readingnook/src/app/groups/groups/groups.component.ts
+++ client/readingnook/src/app/groups/groups/groups.component.ts
@@ -11,7 +11,28 @@ export class GroupsComponent implements OnInit {
   groups: Group[];
 
   constructor() {
-    this.groups = [];
+
+    const group1 = new Group();
+    group1.genreName = "Horror";
+    group1.groupName = "Horror enthusiasts";
+    group1.id = 1;
+    group1.maxGroupSize = 20;
+    group1.members = [];
+    group1.sponsorEmail = "test@test.com";
+    group1.sponsorName = "Joe Johnson";
+    group1.sponsorPhone = "888-888-8888";
+
+    const group2 = new Group();
+    group2.genreName = "Horror";
+    group2.groupName = "Horror enthusiasts";
+    group2.id = 1;
+    group2.maxGroupSize = 20;
+    group2.members = [];
+    group2.sponsorEmail = "test@test.com";
+    group2.sponsorName = "Joe Johnson";
+    group2.sponsorPhone = "888-888-8888";
+
+    this.groups = [group1, group2];
   }
 
   ngOnInit(): void {
